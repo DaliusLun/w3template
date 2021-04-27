e713703f13c5f136d1b6649b9b09faf31549c92f
DaliusLun <80707358+DaliusLun@users.noreply.github.com>
2021-04-27 15:42:12 +0300

    burger close icon

--- js/main.js
+++ js/main.js
@@ -7,10 +7,13 @@ burger.addEventListener('click', () => {
 
     
     if (navg.className == 'nav on') {
-        navg.classList.remove('on'); 
+        navg.classList.remove('on');
+        burger.classList.add('fa-bars');
+        burger.classList.remove('fa-times'); 
     } else {
         navg.classList.add('on');
-
+        burger.classList.remove('fa-bars');
+        burger.classList.add('fa-times'); 
     }
     
     
