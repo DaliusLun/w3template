f3e87dbe2e2806c19d566f8accdabf482ac9f1db
DaliusLun <80707358+DaliusLun@users.noreply.github.com>
2021-04-29 17:12:23 +0300

    almost done

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 let burger = document.querySelector('.burger');
 let navg = document.querySelector('.nav');
+let head = document.querySelector('.header');
 
 burger.addEventListener('click', () => {
 
@@ -9,11 +10,13 @@ burger.addEventListener('click', () => {
     if (navg.className == 'nav on') {
         navg.classList.remove('on');
         burger.classList.add('fa-bars');
-        burger.classList.remove('fa-times'); 
+        burger.classList.remove('fa-times');
+        
     } else {
         navg.classList.add('on');
         burger.classList.remove('fa-bars');
-        burger.classList.add('fa-times'); 
+        burger.classList.add('fa-times');
+        
     }
     
     
